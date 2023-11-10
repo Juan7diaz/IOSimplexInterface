f194180173da06fcb638987b9bc144d68c7f948d
GMStaworld <Gianmarco.astori.p@gmai.com>
2023-11-09 19:53:57 -0500

    Rearranged file structure

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,9 +1,9 @@
 import Header from "../components/header/Header"
 function LandingPage() {
     return (
-        <div>
+        <div className="w-96">
             <Header />
-            <input type="text" className="block w-full text-sm text-slate-500" value="algo"></input>
+            <input type="text" className="block w-full text-sm text-slate-500" value="Expresión..."></input>
         </div>
     )
 }
