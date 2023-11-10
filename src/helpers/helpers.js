891ee3d1e60a4d34ef3db228a4c11fe6759f1501
“JuanDiaz” <juanluisdiazguerrero@gmail.com>
2023-11-10 00:57:54 -0500

    nueva funcion que parsea la funcion objetivo

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,27 +1,40 @@
 import * as math from 'mathjs'
 
-
 export function parseObjectiveFunction(str = "", type){
   const equation = str.trim()
+  const equationParsed = math.parse(equation)
 
-  const parsed = math.parse(equation)
+  const typeFormated = getFormatedType(type);
+  const n_variables = getcantVariables(equationParsed);
+  const operators = getOperators(equation)
+  const coefficients = getCoefficients(equationParsed, operators)
+  const obj_func = obj_function_parse(typeFormated, coefficients)
 
-  const variables = parsed.filter(node => node.isSymbolNode).map(node => node.name);
+  return { n_variables, obj_func };
+}
 
-  let coefficients = parsed.filter(node => node.isConstantNode).map(node => node.value);
+const obj_function_parse = (type, coefficients) => {
+  const arr = [type, coefficients]
+  const arrJson = JSON.stringify(arr)
+  return arrJson.replaceAll('[', '(').replaceAll(']', ')')
+}
 
-  const operators = equation.split(/[\d\w]+/).filter(op => op !== '').map(op => op.trim());
+const getFormatedType = (type) => type === 'maxZ' ? 'max' : 'min';
 
-  equation[0] !== '-' && operators.unshift('+')
+const getcantVariables = (equationParsed) => {
+  const arr_variables = equationParsed.filter(node => node.isSymbolNode).map(node => node.name);
+  const cant_variables = arr_variables.length;
+  return cant_variables;
+}
 
+const getCoefficients = (equationParsed, operators) => {
+  let coefficients = equationParsed.filter(node => node.isConstantNode).map(node => node.value);
   coefficients = coefficients.map((coefficient, i) => operators[i] === '-' ? -coefficient : coefficient)
+  return coefficients;
+}
 
-  return {
-    type,
-    equation,
-    variables,
-    coefficients,
-    operators
-  };
-
+const getOperators = (equation) => {
+  const operators = equation.split(/[\d\w]+/).filter(op => op !== '').map(op => op.trim());
+  equation[0] !== '-' && operators.unshift('+')
+  return operators;
 }
